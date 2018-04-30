7370193bb98af6982316ed22dd60da60157f4430
estef150892 <estefania.desiree.avalos.rivera@gmail.com>
2018-04-30 09:39:01 -0500

    Update WakeRST_DoorSensor.ino
    Se añadio descripción del programa

--- WakeRST_DoorSensor.ino
+++ WakeRST_DoorSensor.ino
@@ -1,3 +1,9 @@
+//27 abril 2018
+//Programa en arduino que despierta al ESP8266. 
+//Recibe los cambios de estado del sensor magnetico de la puerta.
+//Manda señales del estado de la puerta al ESP8266
+
+
 const int wakeESP = 11;//send low pulse to RST to wake
 const int door = 2;//INT0 Pin 2. read state of door sensor
 const int LED = 13; // tell esp if open or close
